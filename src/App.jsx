90d733d6e6efdf75c4a02b581978931fa0dfd8e8
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-04-29 12:59:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+// import DaisyNav from "./components/Navbar/DaisyNav";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return <>
+  
+  <Navbar></Navbar>
+
+
+  {/* <DaisyNav></DaisyNav> */}
+  
+  </>;
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import { Menu, X } from "lucide-react";
+import Link from "./Link";
+import { useState } from "react";
+
+const navData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+
+  {
+    id: 2,
+
+    name: "About",
+    path: "/about",
+  },
+
+  {
+    id: 3,
+    name: "Projects",
+    path: "/projects",
+  },
+
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog",
+  },
+
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const links = navData.map((route) => (
+    <Link key={route.id} route={route}></Link>
+  ));
+
+  return (
+    <nav className="flex justify-between mx-10 mt-4 font-bold text-2xl">
+      <span className="flex" onClick={() => setOpen(!open)}>
+        {open ? (
+          <X className="md:hidden"></X>
+        ) : (
+          <Menu className="md:hidden"></Menu>
+        )}
+
+        <ul className={`md:hidden absolute duration-1000 ${open? 'top-8' : '-top-40'}
+        bg-amber-200
+        `}>
+            {links}
+        </ul>
+
+        <h3 className="ml-4">My Navbar</h3>
+      </span>
+      <ul className="md:flex hidden">{links}</ul>
+      <button>Sign In</button>
+    </nav>
+  );
+};
+
+export default Navbar;
